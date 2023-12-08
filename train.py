f40a798c2c84cfabc68e26a38f26d4ce2e5cf831
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2023-12-08 21:07:53 +0900

    initial commit

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ class DataSet:
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_path', type=str, default="example_data\\tweet_emotions.xlsx", help='place where the dataset is in.')
+parser.add_argument('--data_path', type=str, default="example_data/한국어_단발성_대화_데이터셋.xlsx", help='place where the dataset is in.')
 parser.add_argument('--model_path', type=str, default="models", help='place where the trained model is saved')
 cmd_args = parser.parse_args()
 
