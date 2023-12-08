d30be4133e7a01374f9c504e0b00c3d4664733e8
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2023-12-08 23:07:36 +0900

    initial commit

--- train.py
+++ train.py
@@ -36,73 +36,72 @@ class DataSet:
         return len(self.get_labels())
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--data_path', type=str, default="example_data/한국어_단발성_대화_데이터셋.xlsx", help='place where the dataset is in.')
-parser.add_argument('--model_path', type=str, default="models", help='place where the trained model is saved')
-cmd_args = parser.parse_args()
-
-args = ClassificationTrainArguments(
-    pretrained_model_name="beomi/kcbert-base",
-    downstream_corpus_name="emote",
-    downstream_corpus_root_dir="example_data",
-    downstream_model_dir=cmd_args.model_path,
-    batch_size=32 if torch.cuda.is_available() else 4,
-    learning_rate=5e-5,
-    max_seq_length=128,
-    epochs=6,
-    tpu_cores=0 if torch.cuda.is_available() else 8,
-    seed=7,
-)
-nlpbook.set_seed(args)
-nlpbook.set_logger(args)
-
-corpus = DataSet(cmd_args=cmd_args)
-tokenizer = BertTokenizer.from_pretrained(
-    args.pretrained_model_name,
-    do_lower_case=False,
-)
-pretrained_model_config = BertConfig.from_pretrained(
-    args.pretrained_model_name,
-    num_labels=corpus.num_labels,
-)
-model = BertForSequenceClassification.from_pretrained(
-        args.pretrained_model_name,
-        config=pretrained_model_config,
-)
-
-train_dataset = ClassificationDataset(
-	args=args,
-	corpus=corpus,
-	mode="train",
-	tokenizer=tokenizer,
-)
-train_dataloader = DataLoader(
-    train_dataset,
-    batch_size=args.batch_size,
-    sampler=RandomSampler(train_dataset, replacement=False),
-    collate_fn=nlpbook.data_collator,
-    drop_last=False,
-    num_workers=args.cpu_workers,
-)
-
-# set dataset for validation
-val_dataset = ClassificationDataset(
-    args=args,
-    corpus=corpus,
-    tokenizer=tokenizer,
-    mode="test",
-)
-val_dataloader = DataLoader(
-    val_dataset,
-    batch_size=args.batch_size,
-    sampler=SequentialSampler(val_dataset),
-    collate_fn=nlpbook.data_collator,
-    drop_last=False,
-    num_workers=args.cpu_workers,
-)
-
-task = ClassificationTask(model, args)
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default="example_data/한국어_단발성_대화_데이터셋.xlsx", help='place where the dataset is in.')
+    parser.add_argument('--model_path', type=str, default="models", help='place where the trained model is saved')
+    cmd_args = parser.parse_args()
+
+    args = ClassificationTrainArguments(
+        pretrained_model_name="beomi/kcbert-base",
+        downstream_corpus_name="emote",
+        downstream_corpus_root_dir="example_data",
+        downstream_model_dir=cmd_args.model_path,
+        batch_size=32 if torch.cuda.is_available() else 4,
+        learning_rate=5e-5,
+        max_seq_length=128,
+        epochs=6,
+        tpu_cores=0 if torch.cuda.is_available() else 8,
+        seed=7,
+    )
+    nlpbook.set_seed(args)
+    nlpbook.set_logger(args)
+    tokenizer = BertTokenizer.from_pretrained(
+        args.pretrained_model_name,
+        do_lower_case=False,
+    )
+
+    corpus = DataSet(cmd_args=cmd_args)
+    train_dataset = ClassificationDataset(
+        args=args,
+        corpus=corpus,
+        mode="train",
+        tokenizer=tokenizer,
+    )
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        sampler=RandomSampler(train_dataset, replacement=False),
+        collate_fn=nlpbook.data_collator,
+        drop_last=False,
+        num_workers=args.cpu_workers,
+    )
+
+    # set dataset for validation
+    val_dataset = ClassificationDataset(
+        args=args,
+        corpus=corpus,
+        tokenizer=tokenizer,
+        mode="test",
+    )
+    val_dataloader = DataLoader(
+        val_dataset,
+        batch_size=args.batch_size,
+        sampler=SequentialSampler(val_dataset),
+        collate_fn=nlpbook.data_collator,
+        drop_last=False,
+        num_workers=args.cpu_workers,
+    )
+    pretrained_model_config = BertConfig.from_pretrained(
+        args.pretrained_model_name,
+        num_labels=corpus.num_labels,
+    )
+    model = BertForSequenceClassification.from_pretrained(
+            args.pretrained_model_name,
+            config=pretrained_model_config,
+    )
+    task = ClassificationTask(model, args)
     trainer = nlpbook.get_trainer(args)
     trainer.fit(
         task,
